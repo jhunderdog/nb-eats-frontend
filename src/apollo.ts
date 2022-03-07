79964e9ff908ff0c3c9213c26623b8496ac29c56
jhunderdog <33382water@gmail.com>
2022-03-07 18:13:17 +0900

    Local Only Fields

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,6 +1,19 @@
-import { InMemoryCache, ApolloClient } from "@apollo/client";
+import { InMemoryCache, ApolloClient, makeVar } from "@apollo/client";
 
+export const isLoggedInVar = makeVar(false);
 export const client = new ApolloClient({
     uri: 'http://localhost:4000/graphql',
-    cache: new InMemoryCache()
+    cache: new InMemoryCache({
+        typePolicies: {
+            Query: {
+                fields: {
+                    isLoggedIn: {
+                        read(){
+                            return isLoggedInVar();
+                        }
+                    }
+                }
+            }
+        }
+    })
 });
