145ec59c4a69d8c898ee308e6e19d16f1bdf9076
jhunderdog <33382water@gmail.com>
2022-03-16 21:33:28 +0900

    Restaurant part One

--- src/components/restaurant.tsx
+++ src/components/restaurant.tsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 interface IRestaurantProps {
     id:string;
     coverImg:string;
     name:string;
     categoryName?:string;
 }
-export const Restaurant: React.FC<IRestaurantProps> = ({coverImg,name,categoryName}) => (
-<div className="flex flex-col">
-<div 
-style={{ backgroundImage: `url(${coverImg})`}}
-className="bg-cover bg-center mb-3 py-28"
-></div>
-<h3 className="text-xl text-medium">{name}</h3>
-<span className="border-t mt-4 py-2 text-xs opacity-50 border-gray-400">
-    {categoryName}
-</span>
-</div>
+export const Restaurant: React.FC<IRestaurantProps> = 
+({
+    id,
+    coverImg,
+    name,
+    categoryName
+}) => (
+   <Link to={`/restaurants/${id}`}>
+    <div className="flex flex-col">
+    <div 
+    style={{ backgroundImage: `url(${coverImg})`}}
+    className="bg-cover bg-center mb-3 py-28"
+    ></div>
+    <h3 className="text-xl text-medium">{name}</h3>
+    <span className="border-t mt-4 py-2 text-xs opacity-50 border-gray-400">
+        {categoryName}
+    </span>
+    </div>
+</Link> 
 );
