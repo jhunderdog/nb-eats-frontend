e8354e1545b37ed1acbaad11b58fc6fe3b4ad9de
jhunderdog <33382water@gmail.com>
2022-03-17 20:54:04 +0900

    Restaurant part Two

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -32,5 +32,17 @@ export const Restaurant = () => {
     }
     );
     console.log(data);
-    return <h1>Restaurant</h1>
+    return <div>
+        <div 
+        className="bg-gray-800 bg-center bg-cover py-48" 
+        style={{backgroundImage:`url(${data?.restaurant.restaurant?.coverImg})`,
+        }}
+        >
+            <div className="bg-white w-6/12 py-4 pl-5 md:w-3/12 md:pl-10 lg:6/12 lg:pl-30 ">
+                <h4 className="text-4xl mb-3">{data?.restaurant.restaurant?.name}</h4>
+                <h5 className="text-sm font-light mb-2">{data?.restaurant.restaurant?.category?.name}</h5>
+                <h6 className="text-sm font-light">{data?.restaurant.restaurant?.address}</h6>
+            </div>
+        </div>
+        </div>
 };
