fdc48fb8b757da304fb28a76801cc3c2f4b4ad47
jhunderdog <33382water@gmail.com>
2022-03-22 09:20:25 +0900

    CreateAccount Tests part Thress

--- src/pages/__test__/create-account.spec.tsx
+++ src/pages/__test__/create-account.spec.tsx
@@ -6,6 +6,18 @@ import { render, waitFor, RenderResult } from "../../test-utils";
 import userEvent from "@testing-library/user-event";
 import { UserRole } from "../../__generated__/globalTypes";
 
+const mockPush = jest.fn();
+jest.mock("react-router-dom", () => {
+    const realModule = jest.requireActual("react-router-dom");
+    return {
+        ...realModule,
+        useHistory: () => {
+            return {
+                push: mockPush,
+            }
+        }
+    }
+})
 
 describe("<CreateAccount />", () => {
     let mockedClient: MockApolloClient;
@@ -82,6 +94,11 @@ describe("<CreateAccount />", () => {
         });
         expect(window.alert).toHaveBeenCalledWith("Account Created! Log in now!");
         const mutationError = getByRole("alert");
+        expect(mockPush).toHaveBeenCalledWith("/");
         expect(mutationError).toHaveTextContent("mutation-error")
     })
+});
+
+afterAll(() => {
+    jest.clearAllMocks();
 })
