6c1f0cb119a501d3c5e8adb1c525ff00c81689f1
jhunderdog <33382water@gmail.com>
2022-03-22 16:22:35 +0900

    Our First Cypress Test

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,7 +1,23 @@
-describe("First Test", () => {
-    it("should go to homepage", () => {
-        cy.visit("http://localhost:3000")
+describe("Log In", () => {
+    it("should see login page", () => {
+        cy.visit("/")
         .title()
         .should("eq", "Login | Nuber Eats");
+    })
+    it("can fill out the form", () => {
+        cy.visit("/")
+        .findByPlaceholderText(/email/i)
+        .type("nico@nomadcoders.co")    
+        .findByPlaceholderText(/password/i)    
+        .type("123123123")
+        .findByRole("button")
+        .should("not.have.class", "pointer-events-none");
+    });
+    it("can see email / password validation errors", ()=> {
+        cy.visit("/")
+        .findByPlaceholderText(/email/i)
+        .type("bad@email")
+        .findByRole("alert")
+        .should("have.text", "Please enter a valid email")
     })
 })
