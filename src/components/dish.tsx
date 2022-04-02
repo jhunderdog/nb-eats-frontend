80a48f2b09b3ba927a307f62580b180961854a2c
jhunderdog <33382water@gmail.com>
2022-04-03 07:56:12 +0900

    Making Order part Three

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -12,6 +12,7 @@ interface IDishProps {
     addItemToOrder?: (dishId: number) => void;
     removeFromOrder?: (dishId: number) => void;
     isSelected?: boolean;
+    addOptionToItem?: (dishId:number, option:any) => void;
 }   
 
 export const Dish:React.FC<IDishProps> = ({
@@ -24,7 +25,8 @@ export const Dish:React.FC<IDishProps> = ({
     options,
     addItemToOrder,
     removeFromOrder,
-    isSelected
+    isSelected,
+    addOptionToItem
 }) => {
     const onClick = () => {
         console.log("clicked");
@@ -43,13 +45,13 @@ export const Dish:React.FC<IDishProps> = ({
       };
     return (
         <div
-        onClick={onClick}
+        
         className={` px-8 py-4 border cursor-pointer  transition-all ${
           isSelected ? "border-gray-800" : " hover:border-gray-800"
         }`}
       >
         <div className="mb-5">
-          <h3 className="text-lg font-medium ">{name}</h3>
+          <h3 className="text-lg font-medium ">{name}{orderStarted && <button onClick={onClick}>{isSelected ? "Remove" : "Add"}</button>}</h3>
           <h4 className="font-medium">{description}</h4>
         </div>
         <span>${price}</span>
@@ -57,7 +59,8 @@ export const Dish:React.FC<IDishProps> = ({
           <div>
             <h5 className="mt-8 mb-3 font-medium">Dish Options:</h5>
             {options?.map((option, index) => (
-              <span className="flex items-center" key={index}>
+              <span 
+              onClick={() => addOptionToItem ? addOptionToItem(id, {name: option.name, extra:option.extra}): null} className="flex border items-center" key={index}>
                 <h6 className="mr-2">{option.name}</h6>
                 <h6 className="text-sm opacity-75">(${option.extra})</h6>
               </span>
