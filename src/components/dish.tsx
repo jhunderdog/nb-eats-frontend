29bd7d3155d8fdca9a58f0ebd381bac77979749d
jhunderdog <33382water@gmail.com>
2022-04-02 19:53:05 +0900

    Making Order part Two

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -27,34 +27,43 @@ export const Dish:React.FC<IDishProps> = ({
     isSelected
 }) => {
     const onClick = () => {
-
-        if(orderStarted){
-            if(!isSelected && addItemToOrder){
-                return addItemToOrder(id);
-            }
-            if(isSelected && removeFromOrder){
-                return removeFromOrder(id);
-            } 
+        console.log("clicked");
+        console.log(id);
+        if (orderStarted) {
+          if (!isSelected && addItemToOrder) {
+            console.log("add");
+            return addItemToOrder(id);
+            
+          }
+          if (isSelected && removeFromOrder) {
+            console.log("remove");
+            return removeFromOrder(id);
+          }
         }
-    }
+      };
     return (
-    <div 
-    onClick={onClick} 
-    className={` px-8 py-4 border cursor-pointer transition-all ${isSelected ? "border-gray-800" : "hover:border-gray-800"}`}
-    >
+        <div
+        onClick={onClick}
+        className={` px-8 py-4 border cursor-pointer  transition-all ${
+          isSelected ? "border-gray-800" : " hover:border-gray-800"
+        }`}
+      >
         <div className="mb-5">
-            <h3 className="text-lg font-medium">{name}</h3>
-            <h4 className="font-medium">{description}</h4>
+          <h3 className="text-lg font-medium ">{name}</h3>
+          <h4 className="font-medium">{description}</h4>
         </div>
-        <span>{price}</span>
-        {isCustomer && options && options?.length !==0 && <div>
+        <span>${price}</span>
+        {isCustomer && options && options?.length !== 0 && (
+          <div>
             <h5 className="mt-8 mb-3 font-medium">Dish Options:</h5>
             {options?.map((option, index) => (
-            <span className="flex items-center" key={index}>
+              <span className="flex items-center" key={index}>
                 <h6 className="mr-2">{option.name}</h6>
-                <h6 className="text-sm opacity-75">({option.extra})</h6>
-            </span>
-            ))}</div>}
-    </div>
+                <h6 className="text-sm opacity-75">(${option.extra})</h6>
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
     )
 }
--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import React, { useState } from "react";
+import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router-dom";
 import { Dish } from "../../components/dish";
 import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
@@ -48,56 +49,68 @@ export const Restaurant = () => {
     }
     );
     const [orderStarted, setOrderStarted] = useState(false);
-    const [orderItems, setOrderItems] = useState<CreateOrderItemInput[]>([]);
-    const triggerStartOrder = () => {
-        setOrderStarted(true);
-    };
-    const isSelected = (dishId: number) => {
-        return Boolean(orderItems.find((order) => order.dishId ===dishId))
+  const [orderItems, setOrderItems] = useState<CreateOrderItemInput[]>([]);
+  const triggerStartOrder = () => {
+    setOrderStarted(true);
+  };
+  const isSelected = (dishId: number) => {
+    return Boolean(orderItems.find((order) => order.dishId === dishId));
+  };
+  const addItemToOrder = (dishId: number) => {
+    if (isSelected(dishId)) {
+      return;
     }
-    const addItemToOrder = (dishId:number) => {
-        if(isSelected(dishId)){
-            return;
-        }
-        setOrderItems(current => [{dishId}, ...current]);
-    };
-    const removeFromOrder = (dishId:number) => {
-        setOrderItems(current => current.filter(dish => dish.dishId !== dishId));
-    }
-    console.log(orderItems);
-    return <div>
-        <div 
-        className="bg-gray-800 bg-center bg-cover py-48" 
-        style={{backgroundImage:`url(${data?.restaurant.restaurant?.coverImg})`,
+    setOrderItems((current) => [{ dishId }, ...current]);
+  };
+  const removeFromOrder = (dishId: number) => {
+    setOrderItems((current) =>
+      current.filter((dish) => dish.dishId !== dishId)
+    );
+  };
+  console.log(orderItems);
+  return (
+    <div>
+      <Helmet>
+        <title>{data?.restaurant.restaurant?.name || ""} | Nuber Eats</title>
+      </Helmet>
+      <div
+        className=" bg-gray-800 bg-center bg-cover py-48"
+        style={{
+          backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})`,
         }}
-        >
-            <div className="bg-white w-6/12 py-4 pl-5 md:w-3/12 md:pl-10 lg:6/12 lg:pl-30 ">
-                <h4 className="text-4xl mb-3">{data?.restaurant.restaurant?.name}</h4>
-                <h5 className="text-sm font-light mb-2">{data?.restaurant.restaurant?.category?.name}</h5>
-                <h6 className="text-sm font-light">{data?.restaurant.restaurant?.address}</h6>
-            </div>
-            </div>
-            <div className="container pb-32 flex flex-col items-end mt-20">
-                <button onClick={triggerStartOrder} className="btn">
-                    {orderStarted ? "Ordering": "Start Order"}
-                </button>
-            <div className="w-full grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
-                    {data?.restaurant.restaurant?.menu.map((dish, index)=> (
-                        <Dish
-                        isSelected={isSelected(dish.id)}
-                        id={dish.id}
-                        orderStarted={orderStarted}
-                        key={index}
-                        name={dish.name}
-                        description={dish.description} 
-                        price={dish.price} 
-                        isCustomer={true} 
-                        options={dish.options}
-                        addItemToOrder={addItemToOrder}
-                        removeFromOrder={removeFromOrder}
-                        />
-                    ))}
-                    </div>
-            </div>
+      >
+        <div className="bg-white w-3/12 py-8 pl-48">
+          <h4 className="text-4xl mb-3">{data?.restaurant.restaurant?.name}</h4>
+          <h5 className="text-sm font-light mb-2">
+            {data?.restaurant.restaurant?.category?.name}
+          </h5>
+          <h6 className="text-sm font-light">
+            {data?.restaurant.restaurant?.address}
+          </h6>
         </div>
+      </div>
+      <div className="container pb-32 flex flex-col items-end mt-20">
+        <button onClick={triggerStartOrder} className="btn px-10">
+          {orderStarted ? "Ordering" : "Start Order"}
+        </button>
+        <div className="w-full grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+          {data?.restaurant.restaurant?.menu.map((dish, index) => (
+            <Dish
+              isSelected={isSelected(dish.id)}
+              id={dish.id}
+              orderStarted={orderStarted}
+              key={index}
+              name={dish.name}
+              description={dish.description}
+              price={dish.price}
+              isCustomer={true}
+              options={dish.options}
+              addItemToOrder={addItemToOrder}
+              removeFromOrder={removeFromOrder}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
