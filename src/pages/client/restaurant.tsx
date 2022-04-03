9b560b7c775a2d936291c15277a6081dcd2d6496
jhunderdog <33382water@gmail.com>
2022-04-03 10:35:59 +0900

    Making Order part Five

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router-dom";
 import { Dish } from "../../components/dish";
+import { DishOption } from "../../components/dish-option";
 import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
 import { CreateOrderItemInput } from "../../__generated__/globalTypes";
 import { restaurant, restaurantVariables } from "../../__generated__/restaurant";
@@ -71,21 +72,35 @@ export const Restaurant = () => {
     );
   };
 
-  const addOptionToItem = (dishId:number, option:any) => {
+  const addOptionToItem = (dishId:number, optionName:string) => {
     if(!isSelected(dishId)){
       return;
     }
     const oldItem = getItem(dishId)
     if(oldItem) {
-      const hasOption = Boolean(oldItem.options?.find((aOption) => aOption.name == option.name));
+      const hasOption = Boolean(oldItem.options?.find((aOption) => aOption.name == optionName));
       if(!hasOption){
         removeFromOrder(dishId)
-        setOrderItems((current) => [{ dishId, options:[option, ...oldItem.options!] }, ...current]);
+        setOrderItems((current) => [{ dishId, options:[{name:optionName}, ...oldItem.options!] }, ...current]);
       }
       
     }
     
   };
+
+  const removeOptionFromItem = (dishId:number, optionName:string) => {
+    if(!isSelected(dishId)){
+      return;
+    }
+    const oldItem = getItem(dishId);
+    if (oldItem) {
+      removeFromOrder(dishId);
+      setOrderItems((current) => [
+        { dishId, options: oldItem.options?.filter((option) => option.name !== optionName)}, ...current])
+      return 
+    }
+  }
+
   const getOptionFromItem = (item: CreateOrderItemInput, optionName:string) => {
     return item.options?.find(option => option.name === optionName)
 
@@ -95,8 +110,9 @@ export const Restaurant = () => {
     if(item){
       return Boolean(getOptionFromItem(item, optionName));
     }
-
+    return false;
   }
+
 
   console.log(orderItems);
   return (
@@ -140,21 +156,15 @@ export const Restaurant = () => {
               removeFromOrder={removeFromOrder}     
             >
               {dish.options?.map((option, index) => (
-              <span 
-              onClick={() => 
-              addOptionToItem ? 
-              addOptionToItem(dish.id, {
-                name: option.name, 
-                extra:option.extra
-                })
-                : null
-                } 
-                className={`"flex border items-center" ${isOptionSelected(dish.id, option.name) ? "border-gray-800": ""}`}
-                key={index}
-                >
-                <h6 className="mr-2">{option.name}</h6>
-                <h6 className="text-sm opacity-75">${option.extra})</h6>
-              </span>
+              <DishOption 
+              key={index}
+              dishId={dish.id}
+              isSelected={isOptionSelected(dish.id, option.name)}
+              name={option.name}
+              extra={option.extra}
+              addOptionToItem={addOptionToItem}
+              removeOptionFromItem={removeOptionFromItem}
+                />
             ))}
             </Dish>
           ))}
