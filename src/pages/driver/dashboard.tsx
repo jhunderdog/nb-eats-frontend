69ea32d556b413a2e868b6f711d972912f52e78b
jhunderdog <33382water@gmail.com>
2022-04-08 08:35:45 +0900

    Driver Dashboard part One

---
+++ src/pages/driver/dashboard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+export const Dashboard = () => {
+    return <div>
+        <div className="overflow-hidden" style={{width:window.innerWidth, height:"95vh"}}>
+        <GoogleMapReact
+            defaultZoom={20}
+            defaultCenter={{
+                lat: 59.95,
+                lng: 30.33,
+            }}
+          bootstrapURLKeys={{ key: "AIzaSyAuHeQR9BSfgqoSB_SUJu9oEhh4jEHfdpc"}}
+          
+        >
+          <h1>hello</h1>
+        </GoogleMapReact>
+        </div>
+    </div>
+}
