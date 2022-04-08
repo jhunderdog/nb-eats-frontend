7cacbeed96149f7eda6c301251c0e889a9535f6e
jhunderdog <33382water@gmail.com>
2022-04-08 13:03:58 +0900

    Driver Dashboard part Three

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -2,48 +2,64 @@ import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
 
 interface ICoords {
-    lat:number;
-    lng:number;
+  lat: number;
+  lng: number;
 }
 
 export const Dashboard = () => {
-
-
-    const [driverCoords, setDriverCoords] = useState<ICoords>({lng: 0, lat:0})
-    //@ts-ignore
-    const onSuccess = ({coords: {latitude, longitude}}: Position) => {
-        setDriverCoords({lat: latitude, lng: longitude});
+  const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
+  const [map, setMap] = useState<any>();
+  const [maps, setMaps] = useState<any>();
+  // @ts-ignore
+  const onSucces = ({ coords: { latitude, longitude } }: Position) => {
+    setDriverCoords({ lat: latitude, lng: longitude });
+  };
+  // @ts-ignore
+  const onError = (error: PositionError) => {
+    console.log(error);
+  };
+  useEffect(() => {
+    navigator.geolocation.watchPosition(onSucces, onError, {
+      enableHighAccuracy: true,
+    });
+  }, []);
+  useEffect(() => {
+    if (map && maps) {
+      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
     }
-    //@ts-ignore
-    const onError = (error: PositionError) => {
-        console.log(error);
-    }
-    useEffect(() => {
-        navigator.geolocation.watchPosition(onSuccess, onError, {
-            enableHighAccuracy: true,
-        })
-    }, []);
-    const onApiLoaded = ({map, maps}: {map: any, maps:any}) => {
-        console.log(map, maps);
-        setTimeout(() => {
-            map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
-        }, 3000)
-    };
-    return <div>
-        <div className="overflow-hidden" style={{width:window.innerWidth, height:"95vh"}}>
+  }, [driverCoords.lat, driverCoords.lng]);
+  const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    setMap(map);
+    setMaps(maps);
+  };
+  return (
+    <div>
+      <div
+        className="overflow-hidden"
+        style={{ width: window.innerWidth, height: "50vh" }}
+      >
         <GoogleMapReact
-        yesIWantToUseGoogleMapApiInternals
-        onGoogleApiLoaded={onApiLoaded}
-            defaultZoom={20}
-            defaultCenter={{
-                lat: 36.58,
-                lng: 125.95,
-            }}
-          bootstrapURLKeys={{ key: "AIzaSyAuHeQR9BSfgqoSB_SUJu9oEhh4jEHfdpc"}}
-          
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={onApiLoaded}
+          defaultZoom={20}
+          draggable={false}
+          defaultCenter={{
+            lat: 36.58,
+            lng: 125.95,
+          }}
+          bootstrapURLKeys={{ key: "AIzaSyAuHeQR9BSfgqoSB_SUJu9oEhh4jEHfdpc" }}
         >
-          
+          <div
+            // @ts-ignore
+            lat={driverCoords.lat}
+            lng={driverCoords.lng}
+            className="text-lg"
+          >
+            🚖
+          </div>
         </GoogleMapReact>
-        </div>
+      </div>
     </div>
-}
+  );
+};
